61b4d63f779940e0ea5e5408125e9e6d5d66415b
Abdiel Corona <abdielitopro4800s@gmail.com>
2026-09-04 07:55:18 -0600

    feat:conectar worker a la base de datos

--- database.py
+++ database.py
@@ -1,18 +1,33 @@
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-# La misma URL que usamos en Alembic
-SQLALCHEMY_DATABASE_URL = "postgresql://pulse_admin:mi_contrasena_segura2908@localhost:5432/pricepulse_mvp"
 
-# El motor que hablará con PostgreSQL
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+SQLALCHEMY_DATABASE_URL = os.getenv(
+    "DATABASE_URL",
+    (
+        "postgresql://pulse_admin:"
+        "mi_contrasena_segura2908"
+        "@localhost:5432/pricepulse_mvp"
+    ),
+)
 
-# El creador de sesiones para interactuar con las tablas
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL,
+    pool_pre_ping=True,
+)
 
-# Esta función abrirá y cerrará la base de datos de forma segura en cada petición
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine,
+)
+
+
 def get_db():
     db = SessionLocal()
+
     try:
         yield db
     finally:
